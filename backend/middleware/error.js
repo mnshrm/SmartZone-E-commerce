92ddea91d8f36c5f42a91834137b829b93793391
mnshrm <asysnsrs665@gmail.com>
2022-09-26 20:07:18 +0530

    removed bugs

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,7 +4,7 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal server error";
 
-  if (err.message === "Cast error") {
+  if (err.name === "CastError") {
     const message = `Resource not found. Invalid_id : ${err.path}`;
     err = new ErrorHandler(message, 404);
   }
