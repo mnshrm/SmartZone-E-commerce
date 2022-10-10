409467e18d8259e6605717d97a8f81e82fa8d06f
mnshrm <asysnsrs665@gmail.com>
2022-10-10 14:17:40 +0530

    Complete backend authentication without forgot password feature

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,9 +5,14 @@ const sendEmail = async (options) => {
     host: "smtp.gmail.com",
     port: 465,
     service: process.env.SMPT_SERVICE,
+    secure: true,
     auth: {
+      type: "OAuth2",
       user: process.env.SMPT_USER,
+      clientId:
+        "312677121766-8erdovb0p09tmrt5mc6358a7s5fkliae.apps.googleusercontent.com",
       pass: process.env.SMPT_PASS,
+      clientSecret: "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",
     },
   });
 
